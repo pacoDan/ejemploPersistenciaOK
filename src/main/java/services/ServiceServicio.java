68e6e73eed897923229ffd82f24c9c5fb5729c6b
Daniel <jhonpaco@frba.utn.edu.ar>
2024-09-07 19:19:49 -0300

    :fire: en  test OK

---
+++ src/main/java/services/ServiceServicio.java
@@ -0,0 +1,110 @@
+package services;
+
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+import models.entities.servicios.Servicio;
+import models.entities.servicios.Tarea;
+import models.repositories.RepositorioDeServicios;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ServiceServicio implements WithSimplePersistenceUnit {
+
+    private RepositorioDeServicios repositorioDeServicios;
+
+    public ServiceServicio() {
+        this.repositorioDeServicios = new RepositorioDeServicios();
+    }
+    public void guardarServicios() {
+        /*Servicio servicio1 = new Servicio();
+        servicio1.setNombre("Abogacia");
+
+        Servicio servicio2 = new Servicio();
+        servicio2.setNombre("Ingenieria en Sistemas");
+
+        beginTransaction();
+        repositorioDeServicios.guardar(servicio1);
+        repositorioDeServicios.guardar(servicio2);
+        commitTransaction();*/
+
+        withTransaction(() -> {
+            Servicio servicio1 = new Servicio();
+            servicio1.setNombre("Abogacia");
+
+            Servicio servicio2 = new Servicio();
+            servicio2.setNombre("Ingenieria en Sistemas");
+
+            repositorioDeServicios.guardar(servicio1);
+            repositorioDeServicios.guardar(servicio2);
+        });
+    }
+
+    public void recuperarServicios() {
+        Optional<Servicio> posibleServicio1 = this.repositorioDeServicios.buscarPorId(1L);
+        if(posibleServicio1.isPresent()) {
+            Servicio servicio1 = posibleServicio1.get();
+            System.out.println(servicio1);
+        }
+
+        /*Servicio servicio1bis = this.repositorioDeServicios.buscarPorId2(1L);
+
+        if(servicio1bis != null) {
+
+        }*/
+
+        List<Servicio> servicios = this.repositorioDeServicios.buscarPorNombre("Ingenieria en Sistemas");
+
+        if(!servicios.isEmpty()) {
+            Servicio servicio2 = servicios.get(0);
+            System.out.println(servicio2);
+        }
+
+    }
+
+    public void recuperarServicios2() {
+        List<Servicio> servicios = this.repositorioDeServicios.buscarTodos();
+        System.out.println(servicios);
+    }
+
+    public void actualizarNombreDeServicios() {
+        List<Servicio> servicios = this.repositorioDeServicios.buscarPorNombre("Ingenieria en Sistemas");
+
+        if(!servicios.isEmpty()) {
+            Servicio servicio1 = servicios.get(0);
+            servicio1.setNombre("Ing. en Sistemas");
+            this.repositorioDeServicios.modificar(servicio1);
+        }
+
+        List<Servicio> servicios2 = this.repositorioDeServicios.buscarPorNombre("Abogacia");
+
+        if(!servicios2.isEmpty()) {
+            Servicio servicio2 = servicios2.get(0);
+            servicio2.setNombre("Abogacía");
+            this.repositorioDeServicios.modificar(servicio2);
+        }
+    }
+
+    public void agregarTareasAServicios() {
+        Optional<Servicio> posibleServicio1 = this.repositorioDeServicios.buscarPorId(1L);
+
+        if(posibleServicio1.isPresent()) {
+            Servicio servicio1 = posibleServicio1.get();
+            servicio1.agregarTareas(
+                    Tarea.builder().nombre("Lectura del caso").build(),
+                    Tarea.builder().nombre("Presentación judicial").build()
+            );
+            this.repositorioDeServicios.modificar(servicio1);
+        }
+
+        Optional<Servicio> posibleServicio2 = this.repositorioDeServicios.buscarPorId(2L);
+
+        if(posibleServicio2.isPresent()) {
+            Servicio servicio2 = posibleServicio2.get();
+            servicio2.agregarTareas(
+                    Tarea.builder().nombre("Testing de Software").build(),
+                    Tarea.builder().nombre("Diseño del modelo de datos").build()
+            );
+            this.repositorioDeServicios.modificar(servicio2);
+        }
+    }
+}
--- src/test/java/db/ContextTest.java
+++ src/test/java/db/ContextTest.java
@@ -2,20 +2,31 @@ package db;
 
 
 import io.github.flbulgarelli.jpa.extras.test.SimplePersistenceTest;
+import main.MainExample;
+import models.repositories.RepositorioDeServicios;
 import org.junit.jupiter.api.Test;
+import services.ServiceServicio;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ContextTest implements SimplePersistenceTest {
-
+  ServiceServicio serviceServicio; //= new ServiceServicio();
   @Test
   void contextUp() {
     assertNotNull(entityManager());
   }
-
+  
   @Test
   void contextUpWithTransaction() throws Exception {
     withTransaction(() -> {
+      serviceServicio= new ServiceServicio();
+
+      serviceServicio.guardarServicios();
+      serviceServicio.recuperarServicios();
+      serviceServicio.agregarTareasAServicios();
+
+      serviceServicio.recuperarServicios2();
+      serviceServicio.actualizarNombreDeServicios();
     });
   }
 
